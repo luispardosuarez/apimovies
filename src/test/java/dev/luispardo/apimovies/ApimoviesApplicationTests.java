5b877c4d428d9de18a7695a32394cce98f5fbc91
Luis Pardo <luispardosuarez@gmail.com>
2023-12-19 09:09:59 +0100

    start repository

---
+++ src/test/java/dev/luispardo/apimovies/ApimoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luispardo.apimovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
